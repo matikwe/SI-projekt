bf5a04991fcaa48bf5605f60c42a22a0ec967165
Mateusz <raczkamateusz7@gmail.com>
2020-12-02 13:24:14 +0100

    index

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
+<title>WWW</title>
 <?php
-echo "test";
-echo "test2";
-echo "test3";
-echo "test5";
-echo "test6";
-echo "test7";
+session_start();
+
+define('_ROOT_PATH', dirname(__FILE__));
+
+$actions = array('start'); //wpisywać stworzone podstrony
+$action = 'start'; //zaczyna od...
+
+if(array_key_exists('action', $_GET))
+{
+    if(in_array($_GET['action'], $actions))
+    {
+        $action = $_GET['action'];
+    }
+    else
+    {
+        $action = 'pageNotFound';
+    }
+}
+
+include(_ROOT_PATH.DIRECTORY_SEPARATOR.'actions'.DIRECTORY_SEPARATOR.$action.'.php');
+include(_ROOT_PATH.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.$action.'.php');
+?>
