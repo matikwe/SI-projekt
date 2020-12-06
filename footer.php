1f8536c8a5d893e38eb8b4ac2cea07aca5a400aa
kkuzmicki <kkuzmicki@wp.pl>
2020-12-07 00:21:15 +0100

    KK

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 ?>
 
 <footer>
-    &#9400; Sekcja 3 Politechnika Śląska
+    &#9400; Sekcja 3 Politechnika Śląska <a href="www.facebook.com">FACEBOOK</a><a href="instagram.com">INSTAGRAM</a><a href="twitter.com">TWITTER</a>
 </footer>
 
 </body>
