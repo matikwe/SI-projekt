11a61b81dbdb7ebd26a087bcfdc17f87e6e3cb04
kkuzmicki <kkuzmicki@wp.pl>
2020-12-07 02:52:13 +0100

    KK

--- views/pageNotFound.php
+++ views/pageNotFound.php
@@ -1,6 +1,12 @@
 <?php
 ?>
 <div class="pageNF">
+    <br />
+    <br />
+    <br />
     <h1>PODANA STRONA NIE ISTNIEJE!</h1>
-    <a href="index.php?action=start.php">Powrót do strony głównej</a>
+    <br />
+    <br />
+    <br />
+    <a href="index.php?action=home">Powrót do strony głównej</a>
 </div>
