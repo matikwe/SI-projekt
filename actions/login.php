bd657a7c9d45d378c0df835f2fa8c164862bedeb
Mateusz <raczkamateusz7@gmail.com>
2020-12-09 14:23:43 +0100

    M-log&rej-actionV2

--- actions/login.php
+++ actions/login.php
@@ -1,10 +1,14 @@
 <?php
 include 'Database.php';
+$_SESSION['emptyLogin'] = "";
+$_SESSION['eLogin'] = "";
+$correctLogin = false;
 
 if(isset($_POST['buttonLogin'])){
 
     if(empty($_POST['login']) || empty($_POST['password'])){
-        echo "Uzupełnij dane logowania";
+        $_SESSION['emptyLogin'] = "Uzupełnij dane logowania";
+        $correctLogin = true;
     }else{
         $database = new Database('blog');
         $query = $database->getHandle()->query('SELECT login, password FROM user WHERE login="'.$_POST['login'].'"');
@@ -13,7 +17,11 @@ if(isset($_POST['buttonLogin'])){
             if(password_verify($_POST['password'], $row['password']) == true){
                 $_SESSION['currentLogin'] = $_POST['login'];
                 header("Location: index.php?action=home");
+                $correctLogin = true;
             }
+        }
+        if($correctLogin == false){
+            $_SESSION['eLogin'] = "Podaj prawidłowe dane logowania";
         }
     }
 }
--- views/login.php
+++ views/login.php
@@ -9,8 +9,17 @@
                 <h2>Login</h2>
                 <form action="index.php?action=login" method="POST">
                     <label>
-                        <input type="text" placeholder="Login" name="login">
+                        <input type="text" placeholder="Login" name="login" value="<?php if(!empty($_POST['login'])) echo $_POST['login']; ?>">
                         <input type="password" placeholder="Hasło" name="password">
+                        <div id="error">
+                        <?php
+                            if(!empty($_SESSION['emptyLogin']))
+                                echo"<p>".$_SESSION['emptyLogin']."</p>";
+
+                            if(!empty($_SESSION['eLogin']))
+                                echo"<p>".$_SESSION['eLogin']."</p>";
+                            ?>
+                        </div>
                         <a href="index.php?action=registration" class="info">Nie masz konta? Zarejestruj się...</a>
                         <input type="submit" value="Zaloguj" class="submit" name="buttonLogin">
                     </label>
