b94fe4876c7491a6275e8f56352218445042231c
kkuzmicki <kkuzmicki@wp.pl>
2020-12-13 00:55:11 +0100

    KK

--- views/pageNotFound.php
+++ views/pageNotFound.php
@@ -9,21 +9,8 @@
 include 'topMenu.php';
 ?>
 <div class="pageNF">
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
+
     <h1>PODANA STRONA NIE ISTNIEJE!</h1>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
+
     <a href="index.php?action=home">Powrót do strony głównej</a>
 </div>
