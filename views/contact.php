937a9b0345112d0b6b33b64df04b6a27ff40e59b
kuczu16 <49654909+kuczu16@users.noreply.github.com>
2020-12-15 18:23:58 +0100

    Ola

--- views/contact.php
+++ views/contact.php
@@ -1,4 +1,4 @@
-<html lang="pl-PL">
+<html lang="pl-PL" xmlns="http://www.w3.org/1999/html">
 <head>
     <link rel="stylesheet" href="./style/style.css">
     <link rel="stylesheet" href="./style/fontello.css">
@@ -12,29 +12,22 @@ include 'topMenu.php';
     <br><br><br><br><br>
     <h1>ZAWSZE CHĘTNIE ODPOWIEMY NA TWOJE PYTANIA </h1>
     <br><br><br>
-    <h2>JAK MOŻESZ SIĘ Z NAMI SKONTAKTOWAĆ?</h2>
-    <br>
-<div class="call">
-    <h3>ZADZWOŃ</h3>
-    785678087
 </div>
-<div class="write">
-    <br>
-    <h3>LUB NAPISZ</h3>
+</div>
+<div class="mail">
+    <div class="form">
+    <form action="mail.php" method="post">
+        <h3>NAPISZ DO NAS</h3><br>
+        <input type="text" placeholder="Imię" id="name" name="name"><br><br>
+        <input type="text" placeholder="Nazwisko" id="surname" name="surname"><br><br>
+        <input type="text" placeholder="Mail" id="mail" name="mail"><br><br>
+        <textarea> </textarea>
 
-    <form action="" method="post">
-        <label for="name">Imię:</label><br><br>
-        <input type="text" id="name" name="name"><br><br>
-        <label for="surname">Nazwisko:</label><br><br>
-        <input type="text" id="surname" name="surname"><br><br>
-        <label for="mail">Adres email:</label><br><br>
-        <input type="text" id="mail" name="mail"><br><br>
-        <label for="text">Co chcesz do nas napisać?</label><br><br>
-        <textarea name="text" id="text" rows="20" cows="150">
-        </textarea><br><br>
-        <input type="submit" name="button" value="Wyślij" />
+
+        <input type="submit" class="submit" name="button" value="Wyślij" />
     </form>
-</div>
+    </div>
+
 </div>
 
 </body>
