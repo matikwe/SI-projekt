98bd6f10fcd5584451366ba79a321ac2aaf9f504
joachSzo <joach.szolc@gmail.com>
2020-12-15 23:55:57 +0100

    JS

---
+++ views/articles.php
@@ -0,0 +1,25 @@
+<html lang="pl-PL">
+<head>
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./style/fontello.css">
+    <title>Logowanie</title>
+</head>
+<body>
+<?php
+include 'topMenu.php';
+?>
+<div class="articles">
+
+    <article id="post-1" class="thumbnail"">
+    <div class="entry-thumbnail">
+        <img width="200" height="200" src="./sample.jpg"  </img>
+        <div class="entry-meta">
+            <span class="posted-on"><a href="" rel="bookmark"><time datetime="">11/12/2020</time><time  datetime="2020-12-11T20:45:04+01:00">13/12/2020</time></a></span>
+        </div>
+        <h1 class="entry-title"><a  rel="bookmark">Article</a></h1>	</header>
+    </div>
+
+    <a href="" class="entry-link"><span class="screen-reader-text">Czytaj dalej <span class="meta-nav">&rarr;</span></span></a>
+    </article>
+
+</div>
--- views/home.php
+++ views/home.php
@@ -1,73 +1,46 @@
-<html lang="pl-PL">
-    <head>
-        <link rel="stylesheet" href="./style/style.css">
-        <link rel="stylesheet" href="./style/fontello.css">
-        <title>Strona główna</title>
-    </head>
-    <body>
-    <?php
-    include 'topMenu.php';
-    ?>
-    <div class="home">
+<html lang="pl-PL" xmlns="http://www.w3.org/1999/html">
+<head>
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./style/fontello.css">
+    <title>Strona główna</title>
+</head>
+<body>
+<?php
+include 'topMenu.php';
+?>
+<div class="home">
 
-            <div class="articles">
-                <div class="header">
-                    <h1>Blog muzyczny</h1>
-                </div>
+    <br><br><br><br>
+    <h3>Strona Główna</h3>
+    <div class="ad1">
+        Reklama
+    </div>
+    <!--
 
-            <article id="post-1" class="thumbnail"">
-                <div class="entry-thumbnail">
-                    <img width="200" height="200" src="./sample.jpg"  </img>
-                    <div class="entry-meta">
-                        <span class="posted-on"><a href="" rel="bookmark"><time datetime="">11/12/2020</time><time  datetime="2020-12-11T20:45:04+01:00">13/12/2020</time></a></span>
-                    </div>
-                    <h1 class="entry-title"><a  rel="bookmark">Article</a></h1>	</header>
-                </div>
-
-                <a href="" class="entry-link"><span class="screen-reader-text">Czytaj dalej <span class="meta-nav">&rarr;</span></span></a>
-            </article>
-
-            <article id="post-2" class="thumbnail"">
-            <div class="entry-thumbnail">
-                <img width="200" height="200" src="./sample.jpg"  </img>
-                <div class="entry-meta">
-                    <span class="posted-on"><a href="" rel="bookmark"><time datetime="">12/12/2020</time><time  datetime="2020-12-12T20:45:04+01:00">13/12/2020</time></a></span>
-                </div>
-                <<h1 class="entry-title"><a  rel="bookmark">Article</a></h1>	</header>
-            </div>
-
-            <a href="" class="entry-link"><span class="screen-reader-text">Czytaj dalej <span class="meta-nav">&rarr;</span></span></a>
-            </article>
-
-            <article id="post-3" class="thumbnail"">
-            <div class="entry-thumbnail">
-                <img width="200" height="200" src="./sample.jpg"  </img>
-                <div class="entry-meta">
-                    <span class="posted-on"><a href="" rel="bookmark"><time datetime="">13/12/2020</time><time  datetime="2020-12-13T20:45:00+01:00">13/12/2020</time></a></span>
-                </div>
-                <h1 class="entry-title"><a  rel="bookmark">Article</a></h1>	</header>
-            </div>
-
-            <a href="" class="entry-link"><span class="screen-reader-text">Czytaj dalej <span class="meta-nav">&rarr;</span></span></a>
-            </article>
-
-            <!-- </div>
-            <div class="numbering">
-                PAGE
-            </div>
-        </div>
-        <div class="ad2">
-            <div class="user">
-                użytkownik
-            </div>
-            <div class="ad">
-                REKLAMA
-            </div>
-            <div class="widgets">
-                widzety
-            </div>
-            -->
-
+    <div class="articles">
+        <div class="article">A1</div>
+        <div class="article">A2</div>
+        <div class="article">A3</div>
+        <div class="article">A4</div>
+        <div class="article">A5</div>
+        <div class="article">A6</div>
+        <div class="numbering">
+            PAGE
         </div>
     </div>
-    </body>
+    <div class="ad2">
+        <div class="user">
+            użytkownik
+        </div>
+        <div class="ad">
+            REKLAMA
+        </div>
+        <div class="widgets">
+            widzety
+        </div>
+    </div>
+    -->
+</div>
+
+
+
