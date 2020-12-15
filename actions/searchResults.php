0cd710e67c4bff8e168d3dbf5f07569ebdf4d204
kkuzmicki <kkuzmicki@wp.pl>
2020-12-15 01:54:20 +0100

    KK

--- actions/searchResults.php
+++ actions/searchResults.php
@@ -1,1 +1,7 @@
 <?php
+
+if(!empty($_POST['searchField'])) {
+    $searchField = $_POST['searchField'];
+} else {
+    $searchField = '-';
+}
--- views/searchResults.php
+++ views/searchResults.php
@@ -1,1 +1,16 @@
+<html lang="pl-PL">
+<head>
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./style/fontello.css">
+    <title>Wyniki wyszukiwania</title>
+</head>
+<body>
 <?php
+include 'topMenu.php';
+?>
+
+<div class="pageNF">
+    <?php
+    echo '<h1>Wyniki wyszukiwania dla: <i>'.$searchField.'</i></h1>';
+    ?>
+</div>
