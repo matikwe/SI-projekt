d9389934ca3ac8b34d2c1ac5b524ffa06d38d21f
Mateusz <raczkamateusz7@gmail.com>
2020-12-17 13:29:55 +0100

    addPostV2

--- actions/addPost.php
+++ actions/addPost.php
@@ -1,23 +1,27 @@
 <?php
+include 'Database.php';
 $_SESSION['errorPost'] = "";
 $_SESSION['errorUrl'] = "";
 $validation = true;
 if(isset($_POST['addPost'])){
-    $post = $_POST['post'];
-    if(strlen($post) > 50){
-        //
+
+    if(strlen($_POST['post']) > 50){
+        $post = $_POST['post'];
     }else{
         $_SESSION['errorPost'] = "Wpisany artykuł jest zbyt krótki. Powinien zawierać 50 znaków.";
         $validation = false;
     }
     if(!empty($_POST['url'])){
-        //
+        $url = $_POST['url'];
     }else{
         $_SESSION['errorUrl'] = "Wpisz link do zdjęcia.";
         $validation = false;
     }
 
     if($validation == true){
-        //wysłanie do bazy
+       $database = new Database('blog');
+       $count = $database->count('artykul')+1;
+       $login = $_SESSION['currentLogin'];
+       $query = $database->getHandle()->query('INSERT INTO artykul(id_artykulu, tresc, autor, zdjecie, akceptacja_admina) VALUES ("' . $count . '","' . $post . '","' . $login . '","' . $url . '","false")');
     }
 }
