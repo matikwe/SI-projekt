7d6582e2e1d80c98d908eec91817815032060dc7
kuczu16 <49654909+kuczu16@users.noreply.github.com>
2020-12-20 20:18:24 +0100

    Ola

--- actions/contactPanel.php
+++ actions/contactPanel.php
@@ -1,1 +1,16 @@
 <?php
+include 'Database.php';
+include 'User.php';
+
+$data = new Database('blog');
+
+$data->count('contact');
+$query = $data->getHandle()->query('SELECT * FROM pytania');
+
+$i = 0;
+foreach ($query as $item){
+    $contact = new Contact ($item['questionId'], $item['name'], $item['surname'], $item['email'], $item['contents'], $item['contact']);
+    $_SESSION['userDisplay'][$i] = serialize($contact);
+    $i++;
+}
+$_SESSION['countContactDisplay'] = $i;
