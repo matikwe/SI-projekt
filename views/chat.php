5f7b6ad58b915e1e877db46bc43f3c721a3926c3
HanaHentu <zubrzyckid96@gmail.com>
2020-12-22 04:03:45 +0100

    nie wiem czy lepszy czat, nie dobrane jeszcze kolory i nie dokonczylem jeszcze przycisku ale wydaje mi sie ze funkcjinalnie lepszy zrobiony w calosci na grid Jutro wieczorem go pelni dokoncze

--- views/chat.php
+++ views/chat.php
@@ -2,36 +2,51 @@
 <head>
     <meta charset="utf-8">
     <title>Czat</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="./style/chat.css"/>
     <link rel="stylesheet" href="./style/style.css">
     <link rel="stylesheet" href="./style/fontello.css">
-
 </head>
 <body>
 
 <?php
 include 'topMenu.php';
 ?>
-<div class="header">
-    <h1>Chat</h1>
-</div>
-<div class="chatContent">
+
+<div id="chat-container">
     <?php
     for ($i = 0; $i < $_SESSION['countMessagesDisplay']; $i++){
     $chat = unserialize($_SESSION['messagesDisplay'][$i]);
     ?>
-    <div class="chatContainer">
-        <img src="./userprofile.jpg" alt="Avatar">
-        <p><?php echo $chat->getMessage();?></p>
-        <span class="time-right"><?php echo $chat->getDate();?></span>
+    <div id="online-list">
+        Online List
     </div>
+    <div id="chat-message-list">
+        <div class="message-row user-message">
+            <div class="message-content">
+                <img src="https://i.pinimg.com/236x/21/87/a8/2187a85893e81c2f964f1c89ef559c4e.jpg" width="128px" alt="User Picture"/>
+                <div class="user-name">Hentu</div>
+                <div class="message-text"><?php echo $chat->getMessage();?></div>
+                <div class="message-time">16.04</div>
+            </div>
+        </div>
+        <div class="message-row other-message">
+            <div class="message-content">
+                <img src="https://i.pinimg.com/236x/21/87/a8/2187a85893e81c2f964f1c89ef559c4e.jpg" width="128px" alt="User Picture"/>
+                <div class="user-name">Hentu</div>
+                <div class="message-text">Ok then</div>
+                <div class="message-time">16.04</div>
+            </div>
+        </div>
 
+
+
+
+    </div>
         <?php
-        }
-        ?>
-</div>
-<div class="chatForm">
-    <form action="index.php?action=chat" method="POST">
-        <input type="text" placeholder="Wprowadz wiadomosc" name="message">
-        <input type="submit" value="Wyslij" class="submit" name="action">
-    </form>
+    }
+    ?>
+    <div id="chat-form">
+        <input type="text" placeholder="Wprowadź wiadomość"/>
+        <img src="https://static.thenounproject.com/png/367821-200.png" width="51px" alt="Wyślij"/>
+    </div>
 </div>
