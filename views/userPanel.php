c185db1bba6374b80bd3633f922bd8a089c9e6ec
kkuzmicki <kkuzmicki@wp.pl>
2020-12-23 03:41:29 +0100

    KK

---
+++ views/userPanel.php
@@ -0,0 +1,17 @@
+<html lang="pl-PL">
+<head>
+    <meta charset="utf-8">
+    <title>O nas</title>
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./style/fontello.css">
+</head>
+<body>
+
+<?php
+include 'topMenu.php';
+?>
+
+<div class="pageNF">
+    <h1>Panel admina</h1>
+    <a href="index.php?action=logout">Wyloguj</a>
+</div>
