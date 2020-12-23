def2c80b5def576490870b6d826b504c2dc3a0df
kkuzmicki <kkuzmicki@wp.pl>
2020-12-23 03:12:54 +0100

    KK

--- topMenu.php
+++ topMenu.php
@@ -4,17 +4,17 @@
     </div>
     <div class="menu">
 
-        <a href="index.php?action=about" class="option">O NAS</a>
+        <a href="index.php?action=home" class="option">STRONA GŁÓWNA</a>
+        <a href="index.php?action=articles"><div class="option">ARTYKUŁY</div></a>
         <a href="index.php?action=chat"><div class="option">CZAT</div></a>
-        <a href="index.php?action=contact"><div class="option">KONTAKT</div></a>
         <?php
         if(empty($_SESSION['currentLogin']))
             echo"<a href='index.php?action=login'><div class='option'>LOGOWANIE</div></a>";
-        else{
+        else {
             echo"<a href='index.php?action=logout'><div class='option'>WYLOGUJ</div></a>";
         }
         ?>
-        <a href="index.php?action=articles"><div class="option">ARTYKUŁY</div></a>
+        <a href="index.php?action=about"><div class="option">O NAS</div></a>
     </div>
 
     <div class="search">
