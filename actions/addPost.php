82c6c2ac18cb7fe2edbaf93acdf42bd24bbaf0ee
Mateusz <raczkamateusz7@gmail.com>
2020-12-29 11:50:57 +0100

    userPanel-AddPost-->finish

--- actions/addPost.php
+++ actions/addPost.php
@@ -6,30 +6,29 @@ $_SESSION['errorTitle'] = "";
 $validation = true;
 if(isset($_POST['addPost'])){
 
-    if(strlen($_POST['post']) > 50){
-        $post = $_POST['post'];
-    }else{
+    if(strlen($_POST['post']) < 50){
         $_SESSION['errorPost'] = "Wpisany artykuł jest zbyt krótki. Powinien zawierać 50 znaków.";
         $validation = false;
     }
-    if(!empty($_POST['url'])){
-        $url = $_POST['url'];
-    }else{
+    if(empty($_POST['url'])){
         $_SESSION['errorUrl'] = "Wpisz link do zdjęcia.";
         $validation = false;
     }
-    if(!empty($_POST['title'])){
-        $title = $_POST['title'];
-    }else{
+    if(empty($_POST['title'])){
         $_SESSION['errorTitle'] = "Wpisz tytuł.";
         $validation = false;
     }
 
     if($validation == true){
-       $database = new Database('blog');
-       $count = $database->count('artykul')+1;
-       $login = $_SESSION['currentLogin'];
-       $query = $database->getHandle()->query('INSERT INTO artykul(id_artykulu, tresc, autor, zdjecie, akceptacja_admina) VALUES ("' . $count . '","'.$title.'","' . $post . '","' . $login . '","' . $url . '","false")');
-       header("Location: index.php?action=userPanel");
+
+        $post = $_POST['post'];
+        $url = $_POST['url'];
+        $title = $_POST['title'];
+
+        $database = new Database('blog');
+        $count = $database->count('artykul')+1;
+        $login = $_SESSION['currentLogin'];
+        $query = $database->getHandle()->query('INSERT INTO artykul(id_artykulu, tytul, tresc, autor, zdjecie, akceptacja_admina) VALUES (' . $count . ',"'.$title.'","' . $post . '","' . $login . '","' . $url . '","false")');
+        header("Location: index.php?action=userPanel");
     }
 }
