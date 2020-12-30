bdece3041d0b12af55a4e48a36d34081ca8d82d9
Mateusz <raczkamateusz7@gmail.com>
2020-12-30 17:28:54 +0100

    article --> finish

--- actions/articles.php
+++ actions/articles.php
@@ -3,24 +3,34 @@ include 'Database.php';
 include "Article.php";
 
 $database = new Database('blog');
+if(empty($_GET['name'])) {
+    $query = $database->getHandle()->query('SELECT * FROM artykul');
+    $i = 0;
+    $j = 0;
+    $k = 0;
+    foreach ($query as $item) {
+        $article = new Article($item['id_artykulu'], $item['tytul'], $item['tresc'], $item['autor'], $item['zdjecie'], $item['akceptacja_admina']);
 
-$query = $database->getHandle()->query('SELECT * FROM artykul');
-$i = 0;
-$j = 0;
-$k = 0;
-foreach ($query as $item) {
-    $article = new Article($item['id_artykulu'], $item['tytul'], $item['tresc'], $item['autor'], $item['zdjecie'], $item['akceptacja_admina']);
-
-    if($item['akceptacja_admina'] == "true") {
-        $_SESSION['articles'][$i] = serialize($article);
-        $i++;
-    }else
-    if($item['akceptacja_admina'] == "false") {
-        $_SESSION['articlesNotAccepted'][$j] = serialize($article);
-        $j++;
+        if ($item['akceptacja_admina'] == "true") {
+            $_SESSION['articles'][$i] = serialize($article);
+            $i++;
+        } else
+            if ($item['akceptacja_admina'] == "false") {
+                $_SESSION['articlesNotAccepted'][$j] = serialize($article);
+                $j++;
+            }
     }
-}
 
-$_SESSION['countArticles'] = $i;
-$_SESSION['countArticlesNotAccepted'] = $j;
-
+    $_SESSION['countArticles'] = $i;
+    $_SESSION['countArticlesNotAccepted'] = $j;
+}else
+    if($_GET['name'] == 'acceptDB') {
+        $currentID = $_GET['postID'];
+        $updateStatus = $database->getHandle()->query('UPDATE artykul SET akceptacja_admina = "true" WHERE id_artykulu = '.$currentID);
+        header('Location: index.php?action=articles');
+}else
+    if($_GET['name'] == 'deleteDB') {
+        $currentID = $_GET['postID'];
+        $updateStatus = $database->getHandle()->query('UPDATE artykul SET akceptacja_admina = "false" WHERE id_artykulu = '.$currentID);
+        header('Location: index.php?action=articles');
+    }
--- views/articles.php
+++ views/articles.php
@@ -9,32 +9,61 @@
 include 'topMenu.php';
 ?>
 <h1>Artykuły</h1>
-<?php
-if (empty($_GET['name']))
-{
+
+    <?php
+        if (empty($_GET['name']) || $_GET['name']=='acceptArticle')
+        {
     ?>
 
     <div class="articles">
+
         <?php
-        for ($i = $_SESSION['countArticles'] - 1; $i >= 0; $i--) {
-            $article = unserialize($_SESSION['articles'][$i]);
+        if(empty($_GET['name'])){
+            $i = $_SESSION['countArticles'] - 1;
+            echo '<a href="index.php?action=articles&name=acceptArticle" class="smallButton">Dodane artykuły</a>';
+        }else if($_GET['name']=='acceptArticle'){
+            $i = $_SESSION['countArticlesNotAccepted'] - 1;
+            echo '<a href="index.php?action=articles" class="smallButton">Powrót do dodanych</a>';
+        }
+        for (; $i >= 0; $i--) {
+            if(empty($_GET['name'])){
+                $article = unserialize($_SESSION['articles'][$i]);
+            }else if($_GET['name']=='acceptArticle'){
+                $article = unserialize($_SESSION['articlesNotAccepted'][$i]);
+            }
+
             ?>
 
             <article>
                 <h1><?php echo $article->getTitle(); ?></h1>
                 <p><img src="<?php echo $article->getImg(); ?>"/>
                     <?php echo substr($article->getContents(), 0, 500) . '...'; ?></p>
-                <h4>
-                    <a href="index.php?action=articles&name=currentPost&postID=<?php echo $article->getIdArticle() ?>&number=<?php echo $i ?>">Czytaj
-                        więcej</a></h4>
+                <?php
+                if(empty($_GET['name'])) ?>
+                    <h4><a href="index.php?action=articles&name=currentPost&postID=<?php echo $article->getIdArticle() ?>&number=<?php echo $i ?>">Czytaj więcej</a></h4>
+
+
                 <h5>Autor: <?php echo $article->getAuthor(); ?></h5>
+                <?php
+                    if(!empty($_GET['name'])){
+                        if(($_GET['name'] == 'acceptArticle')&&($_SESSION['currentRole'] == 'admin' || $_SESSION['currentRole'] == 'redaktor')){
+                ?>
+                        <a href="index.php?action=articles&name=acceptDB&postID=<?php echo $article->getIdArticle() ?>" class="smallButton">Akceptuj</a>
+                <?php
+                    }
+                }else if(($_SESSION['currentRole'] == 'admin' || $_SESSION['currentRole'] == 'redaktor')){
+                ?>
+                        <a href="index.php?action=articles&name=deleteDB&postID=<?php echo $article->getIdArticle() ?>" class="smallButton">Usuń z głównej</a>
+                <?php
+                }
+                ?>
             </article>
 
             <?php
         } ?>
     </div>
     <?php
-}else
+}else if($_GET['name'] == 'currentPost')
 {
 $number = $_GET['number'];
 $article = unserialize($_SESSION['articles'][$number]);
@@ -44,10 +73,26 @@ $article = unserialize($_SESSION['articles'][$number]);
         <h1><?php echo $article->getTitle(); ?></h1>
         <p><img src="<?php echo $article->getImg(); ?>"/>
             <?php echo $article->getContents(); ?></p>
-        <!--<h4><a href="index.php?action=articles&name=currentPost&postID=<?php echo $article->getIdArticle() ?>&number=<?php echo $i ?>">Czytaj więcej</a></h4>-->
         <h5>Autor: <?php echo $article->getAuthor(); ?></h5>
     </article>
+
 </div>
-<?php
+
+
+    <?php
+
+    $next = $number+1;
+    $back = $number-1;
+    $max = $_SESSION['countArticles'] - 1;
+
+    if($back >= 0){
+    ?>
+        <a href="index.php?action=articles&name=currentPost&number=<?php echo $back?>" class="smallButton">Poprzedni artykuł</a>
+    <?php }
+    if($next <= $max){
+    ?>
+        <a href="index.php?action=articles&name=currentPost&number=<?php echo $next?>" class="smallButton">Następny artykuł</a>
+    <?php
+    }
 }
 ?>
