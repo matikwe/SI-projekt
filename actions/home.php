2b48f65206ff9b82a5d19df6a21c4aea4b960cf3
kkuzmicki <kkuzmicki@wp.pl>
2021-01-06 01:55:30 +0100

    KK

--- actions/home.php
+++ actions/home.php
@@ -1,1 +1,17 @@
 <?php
+include 'Database.php';
+
+$database = new Database("blog");
+$count = $database->count('ciekawostki');
+
+echo $count;
+$day = date("d");
+$idFF = 1;
+
+for($i = 0; $i < $day; $i++) {
+    $idFF++;
+    if($idFF > $count) $idFF = 1;
+}
+
+echo 'Numer: '.$idFF;
+$query = $database->getHandle()->query('SELECT treść FROM ciekawostki WHERE id_ciekawostki='.$idFF);
