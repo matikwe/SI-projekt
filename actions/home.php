cfa7e2d7f03796dc52ac315796692a45c3fde016
kkuzmicki <kkuzmicki@wp.pl>
2021-01-15 23:17:06 +0100

    KK

--- actions/home.php
+++ actions/home.php
@@ -1,5 +1,7 @@
 <?php
 include 'Database.php';
+include 'Article.php';
+include 'User.php';
 
 $database = new Database("blog");
 $count = $database->count('ciekawostki');
@@ -13,3 +15,17 @@ for($i = 0; $i < $day; $i++) {
 }
 
 $query = $database->getHandle()->query('SELECT treść FROM ciekawostki WHERE id_ciekawostki='.$idFF);
+
+$newArticles = $database->getHandle()->query("SELECT tytul, tresc FROM artykul WHERE akceptacja_admina LIKE 'true' ORDER BY id_artykulu DESC LIMIT 3");
+
+$index = 0;
+foreach($newArticles AS $row) {
+    $object[$index] = new Article(0, $row['tytul'], $row['tresc'], '', '', 1);
+    $index++;
+}
+$index = 0;
+$newUsers = $database->getHandle()->query('SELECT login, opis FROM user');
+foreach($newUsers AS $row) {
+    $nU[$index] = new User(0, $row['login'], '', '', '', $row['opis'], '');
+    $index++;
+}
--- views/home.php
+++ views/home.php
@@ -13,7 +13,7 @@ include 'topMenu.php';
 
 <h1>Strona główna</h1>
 
-<main style="height: 1600px;">
+<main style="height: 2100px;">
     <div class="newArticles hints">
         <h2 class="hints"><i class="icon-lightbulb hints"></i>Czy wiesz, że...</h2>
         <?php
@@ -37,25 +37,67 @@ include 'topMenu.php';
     <div class="newArticles new">
         <h2 class="new"><i class="icon-plus new"></i>Nowe artykuły</h2>
         <article>
-            <h3>Nowy 1</h3>
+            <h3>
+                <?php
+                    echo $object[0]->getTitle();
+                ?>
+            </h3>
+            <?php echo substr($object[0]->getContents(), 0, 80).'...'; ?>
         </article>
         <article>
-            <h3>Nowy 2</h3>
+            <h3>
+                <?php
+                    echo $object[1]->getTitle();
+                ?>
+            </h3>
+            <?php echo substr($object[1]->getContents(), 0, 80).'...'; ?>
         </article>
         <article>
-            <h3>Nowy 3</h3>
+            <h3>
+                <?php
+                echo $object[2]->getTitle();
+                ?>
+            </h3>
+            <?php echo substr($object[2]->getContents(), 0, 80).'...'; ?>
         </article>
     </div>
     <div class="newArticles newUsers">
         <h2 class="newUsers"><i class="icon-user newUsers"></i>Nowi użytkownicy</h2>
         <article>
-            <h3>Użytkownik 1</h3>
+            <h3>
+                <?php
+                echo $nU[0]->getLogin();
+                ?>
+            </h3>
+            <?php
+            echo $nU[0]->getDescription();
+            ?>
         </article>
         <article>
-            <h3>Użytkownik 2</h3>
+            <h3>
+                <?php
+                echo $nU[1]->getLogin();
+                ?>
+            </h3>
+            <?php
+            echo $nU[1]->getDescription();
+            ?>
         </article>
         <article>
-            <h3>Użytkownik 3</h3>
+            <h3>
+                <?php
+                echo $nU[2]->getLogin();
+                ?>
+            </h3>
+            <?php
+            echo $nU[2]->getDescription();
+            ?>
         </article>
     </div>
 </main>
+<?php
+$index = 0;
+    for($i = 0; $i < 3; $i++) {
+        echo $object[$i]->getTitle();
+    }
+?>
